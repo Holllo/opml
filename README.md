8279e43330f34bd95cfe26317fbe79028dcb3df6
Bauke <me@bauke.xyz>
2020-08-28 13:37:29 +0200

    Consolidate the API and CLI into one repository with Cargo workspaces.

--- README.md
+++ README.md
@@ -1,31 +1,21 @@
 # OPML
 
-> An OPML parser for Rust.
+> An OPML parser for Rust and the command-line.
 
-## Getting Started
+## API
 
-```rust
-use opml::OPML;
-
-let xml = r#"<opml version="2.0"><body><outline text="Outline"/></body></opml>"#;
-let parsed = OPML::new(xml).unwrap();
-
-println!("{:#?}", parsed);
-```
-
-Check out [the documentation](https://docs.rs/opml/) for further details.
+For the API documentation, see [docs.rs](https://docs.rs/opml).
 
 ## CLI
 
-Looking for a command-line utility to parse OPML documents? Check out [the `opml-cli`](https://git.holllo.cc/Holllo/opml-cli/), a small wrapper around this crate that will let you do just that.
+To install the OPML command-line parser, run `cargo install opml_cli` or download a precompiled executable directly from [the Releases page](https://github.com/Holllo/opml/releases/latest).
+
+Then run `opml --help` to see all the available options.
 
 ## License
 
-Open-sourced with either the
+Both the API and command-line parser are licensed under either of [Apache License, Version 2.0](https://github.com/Holllo/opml/blob/main/LICENSE-Apache) or [MIT license](https://github.com/Holllo/opml/blob/main/LICENSE-MIT) at your option.
 
-* [Apache License, Version 2.0](https://git.holllo.cc/Holllo/opml/src/branch/main/LICENSE-Apache) (http://www.apache.org/licenses/LICENSE-2.0)
-* [MIT license](https://git.holllo.cc/Holllo/opml/src/branch/main/LICENSE-MIT) (http://opensource.org/licenses/MIT)
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in either crate by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
 
-at your option.
-
-The samples [located in `tests/spec_samples`](https://git.holllo.cc/Holllo/opml/src/branch/main/tests/spec_samples) were [taken from the OPML 2.0 spec](http://dev.opml.org/spec2.html#examples) and are subject to [their own license](https://git.holllo.cc/Holllo/opml/src/branch/main/tests/spec_samples/LICENSE).
+The samples [located in `opml_api/tests/spec_samples`](https://github.com/Holllo/opml/tree/main/opml_api/tests/spec_samples) were [taken from the OPML 2.0 spec](http://dev.opml.org/spec2.html#examples) and are subject to [their own license](https://github.com/Holllo/opml/tree/main/opml_api/tests/spec_samples).
---
+++ opml_cli/source/main.rs
@@ -0,0 +1,110 @@
+use std::fs::read_to_string;
+
+use clap::{
+  crate_authors, crate_description, crate_version, App, Arg, ArgGroup,
+};
+use opml::{Outline, OPML};
+
+fn main() {
+  let cli = App::new("OPML CLI")
+    .about(crate_description!())
+    .author(crate_authors!())
+    .version(crate_version!())
+    .args(&[
+      // Format flags.
+      Arg::with_name("json")
+        .long("json")
+        .long_help("Output the OPML as JSON.")
+        .takes_value(false),
+      Arg::with_name("json pretty")
+        .long("json-pretty")
+        .long_help("Output the OPML as pretty-printed JSON.")
+        .takes_value(false),
+      Arg::with_name("rss")
+        .long("rss")
+        .long_help(
+          "Only output the outline text and xmlUrl attributes \
+          when both are present in the outline element.",
+        )
+        .takes_value(false),
+      // Boolean flags.
+      Arg::with_name("verbose")
+        .long("verbose")
+        .long_help("Print extra information while running.")
+        .takes_value(false),
+      // Options that are only allowed once.
+      Arg::with_name("file")
+        .long("file")
+        .long_help("The OPML file to parse.")
+        .required(true)
+        .short("f")
+        .takes_value(true),
+    ])
+    .group(
+      ArgGroup::with_name("format")
+        .args(&["json", "json pretty", "rss"])
+        .required(true),
+    )
+    .get_matches();
+
+  // Extract format flags.
+  let json = cli.is_present("json");
+  let json_pretty = cli.is_present("json pretty");
+  let rss = cli.is_present("rss");
+
+  // Extract boolean flags.
+  let verbose = cli.is_present("verbose");
+
+  // Extract the various options.
+  let file = cli.value_of("file").unwrap();
+
+  // Read the file to string.
+  let xml = read_to_string(file).expect("Failed to read OPML file");
+
+  // Parse the OPML from the read file.
+  let opml = OPML::new(&xml).expect("Failed to parse OPML file");
+
+  if rss {
+    // Get all the outlines from the OPML document.
+    let outlines = extract_all_outlines(&opml.body.outlines);
+
+    // Print out the text and xmlUrl attributes when possible.
+    for outline in outlines {
+      if let Some(xml_url) = outline.xml_url {
+        println!("{}", outline.text);
+        println!("{}", xml_url);
+      } else if verbose {
+        println!(
+          "Skipping \"{}\" because it did not have an xmlUrl attribute.",
+          outline.text
+        );
+      }
+    }
+  } else if json {
+    println!(
+      "{}",
+      serde_json::to_string(&opml).expect("Failed to convert OPML to JSON")
+    );
+  } else if json_pretty {
+    println!(
+      "{}",
+      serde_json::to_string_pretty(&opml)
+        .expect("Failed to convert OPML to pretty JSON")
+    );
+  } else {
+    unreachable!();
+  }
+}
+
+/// A helper function that takes in `opml::Outline` elements and returns all
+/// children it can find in a single `Vec<Outline>`.
+pub fn extract_all_outlines(outlines: &[Outline]) -> Vec<Outline> {
+  let mut accumulator = vec![];
+
+  for outline in outlines {
+    accumulator.push(outline.clone());
+    accumulator.append(&mut extract_all_outlines(&outline.outlines));
+  }
+
+  accumulator
+}
